78b419ef9e16d09560d166d31eb2be3f15561e76
Jon Wood <jon@blankpad.net>
2022-07-08 09:53:44 +0000

    Generate projects context

---
+++ priv/repo/migrations/20220708094037_create_projects.exs
@@ -0,0 +1,11 @@
+defmodule ProjectManager.Repo.Migrations.CreateProjects do
+  use Ecto.Migration
+
+  def change do
+    create table(:projects) do
+      add :name, :string
+
+      timestamps()
+    end
+  end
+end

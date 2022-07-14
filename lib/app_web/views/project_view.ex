13bd94716d27e539b80015ad0daea0f150c27763
Jon Wood <jon@blankpad.net>
2022-07-14 06:53:26 +0000

    Allow deletion of projects

--- lib/app_web/views/project_view.ex
+++ lib/app_web/views/project_view.ex
@@ -1,7 +1,3 @@
 defmodule ProjectManagerWeb.ProjectView do
   use ProjectManagerWeb, :view
-
-  def format_time(time_to_format) do
-    Calendar.strftime(time_to_format, "%H:%M %d %b %Y")
-  end
 end
